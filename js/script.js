38a30f824823435d338417aff57addf677d4c98a
ali raza sulehria <alirazasulehria@gmail.com>
2019-10-31 13:17:05 +0100

    ArraysinArrays

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var heroUndEnemy =[["Batman" ,"The Joker"],
+                   ["Professor X" ,"Magneto"],
+                   ["Thor" ,"Loki"],
+                   ["Superman", "Lex Luthor"],
+                   ["Wolverine","Sabretooth"],
+                   ["Ghost Rider" , "Mephisto"]];
+
+console.log(heroUndEnemy);
